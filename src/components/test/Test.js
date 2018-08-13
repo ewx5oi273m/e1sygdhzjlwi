c1c6a2c62e1d3dfe7303339cf2a80f77a9019c5d
John <mail@example.com>
2018-08-13 12:16:48 +0700

    Section 7 - Lifecycle, HTTP _ Deployment\2 - GET Requests

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'hello'
+    title: '',
+    body: ''
   };
 
   componentDidMount() {
-    console.log('componentDidMount...');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({title: data.title, body: data.body}));
   }
 
   // componentWillMount() {
@@ -17,18 +20,20 @@ class Test extends Component {
   //   console.log('componentWillUpdate...');
   // }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return null;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('componentWillUpdate...');
-  }
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return null;
+  // }
+  //
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('componentWillUpdate...');
+  // }
 
   render() {
+    const {title, body} = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
